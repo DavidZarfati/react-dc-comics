cf34f851908f1c63af958c9cd5c809042500a0a7
DavidZarfati <zarda2001@gmail.com>
2025-11-26 10:04:56 +0100

    feat:aggiunta active

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
                 <div className="header-destro d-flex">
                     <ul className="d-flex">
                         <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
+                        <li><a href="#" className="active">Comics</a></li>
                         <li><a href="#">Movies</a></li>
                         <li><a href="#">Tv</a></li>
                         <li><a href="#">Games</a></li>
