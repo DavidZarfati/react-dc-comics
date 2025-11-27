dd89d0ad5c69cc77cb9bf49865ef16f17ffd4f08
DavidZarfati <zarda2001@gmail.com>
2025-11-27 16:40:36 +0100

    feat:destrutturazione del componente crea Card

--- components/CreaCard.jsx
+++ components/CreaCard.jsx
@@ -1,10 +1,21 @@
 
 
-export default function CreaCard(props) {
+// export default function CreaCard(props) {
+//     return (
+//         <div className={props.className}>
+//             <img src={props.thumb} alt={props.series} />
+//             <h5>{props.series}</h5>
+//         </div>
+//     );
+// }
+
+// Possiamo DESTRUTTURARE?
+
+export default function CreaCard({ className, thumb, series }) {
     return (
-        <div className={props.className}>
-            <img src={props.thumb} alt={props.series} />
-            <h5>{props.series}</h5>
+        <div className={className}>
+            <img src={thumb} alt={series} />
+            <h5>{series}</h5>
         </div>
     );
 }
